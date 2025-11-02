676d510cd7955eb408a133408d40de7c82942481
Hassan-ALqatawi <alqatawih@gmail.com>
2025-11-02 02:11:20 +0200

    update file sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,7 +12,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1.0,
     },
     {
-      url: `${baseUrl}/breeding-guide`,
+      url: `${baseUrl}/chick-guidelines`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
